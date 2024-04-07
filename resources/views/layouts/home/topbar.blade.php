3dd55d4f13d09ed6bb80cddf897274ed6408c28f
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-04-07 23:55:15 +0800

    update3

--- resources/views/layouts/home/topbar.blade.php
+++ resources/views/layouts/home/topbar.blade.php
@@ -4,17 +4,11 @@
     <a href="#" class="sidebar-toggler flex-shrink-0">
       <i class="fa fa-bars"></i>
     </a>
-    <form class="d-none d-md-flex ms-4">
-      <input class="form-control border-0" type="search" placeholder="Search">
-    </form>
+  
     <div class="navbar-nav align-items-center ms-auto">
       <div class="nav-item dropdown">
-        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-          <i class="fa fa-bell me-lg-2"></i>
-          <span class="d-none d-lg-inline-flex">Notification</span>
-        </a>
-        <!-- Notification Dropdown Menu -->
-        <!-- Notification Dropdown Menu Ends -->
+       
+        
       </div>
 
         <div >
